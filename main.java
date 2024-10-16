f5210adc3fe221d2ff34e88019e9dd90d8e87393
Owen Merrill <91266947+TheChozoLord@users.noreply.github.com>
2024-10-16 10:50:25 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,13 @@
+package OOP16;
+public class reservation {(
+}String customerName;
+double reservationTime;
+int numGuests;
+
+}
+public normalReservation() {
+}
+public VIPReservation{
+        int VIPID
+}
+}
